fafdb010afd9d606ce5198e085f65fa1199b6ff0
fange123 <2030564240@qq.com>
2022-09-19 18:00:15 +0800

    feat:express-validator 表单验证

--- router/user.js
+++ router/user.js
@@ -3,12 +3,25 @@ const Result = require("../models/result");
 const { login } = require("../services/user");
 const { md5 }  = require("../utils");
 const PWD_SALE  = require("../utils/constant")
+const boom = require("boom")
+const { body,validationResult } = require("express-validator")
 
 const router = express.Router();
 
 //* 创建登录路由
-router.post('/login', function(req, result){
-  const {username,password} = req.body
+router.post('/login',[
+  body('username').isString().withMessage('用户名类型不正确'),
+  body('password').isNumeric().withMessage('密码类型不正确'),
+], function(req, result,next){
+  const err = validationResult(req)
+  if(! err.isEmpty()){
+    //* 拿到错误的msg
+    const [{msg}] = err.errors
+    //* 通过next传递给下一个中间件boom让它去处理
+      next(boom.badRequest(msg))
+  }else{
+
+    const {username,password} = req.body
   const md5Password = md5(`${password}${PWD_SALE}`)
   login(username,md5Password).then(res=> {
      if(!res || res.length === 0){
@@ -18,6 +31,8 @@ router.post('/login', function(req, result){
 
   }
   })
+  }
+
 
 
 
