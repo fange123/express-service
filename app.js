f6f3e039c9e0189298ecb7b8cf871992fd7a45a7
fange123 <2030564240@qq.com>
2022-09-19 15:12:51 +0800

    feat:cors跨域处理

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 // https://github.com/expressjs/express
 const express = require("express");
+// * body-parser 解析body的中间价
+const bodyParser = require("body-parser");
 const router = require("./router");
 const app = express();
+const cors = require("cors");
 
-app.use("/", router);
 
-app.listen(80, () => {
-  console.log("服务起来了，在http://localhost");
+app.use(cors());
+
+app.use(bodyParser.json());//解析body
+app.use(bodyParser.urlencoded({ extended: true }));//解析参数
+
+app.use("/api", router);
+
+app.listen(8089, () => {
+
+  console.log("服务起来了，在http://localhost:8089");
 });
--- router/user.js
+++ router/user.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const Result = require("../models/result");
 
 const router = express.Router();
+
+//* 创建登录路由
+router.post('/login', function(req, res){
+  const {username,password} = req.body
+  if(username === 'admin' && password === '123456'){
+    new Result('登录成功').success(res)
+  }else{
+  new Result('登录失败').fail(res)
+  }
+
+})
 
 router.get("/info", (req, res) => {
   res.send("info...");
