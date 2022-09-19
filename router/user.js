e9c86826b0028c7e7468eca122e3670ce385c282
fange123 <2030564240@qq.com>
2022-09-19 18:00:55 +0800

    fix:修改密码校验规则

--- router/user.js
+++ router/user.js
@@ -11,7 +11,7 @@ const router = express.Router();
 //* 创建登录路由
 router.post('/login',[
   body('username').isString().withMessage('用户名类型不正确'),
-  body('password').isNumeric().withMessage('密码类型不正确'),
+  body('password').isString().withMessage('密码类型不正确'),
 ], function(req, result,next){
   const err = validationResult(req)
   if(! err.isEmpty()){
